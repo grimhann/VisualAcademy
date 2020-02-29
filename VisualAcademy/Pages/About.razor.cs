dc9b567827bb52a14c8d2b0c1767a4f2e655c38c
Sungchool Park <grimhan2@gmail.com>
2020-03-01 03:41:52 +0900

    10장

---
+++ VisualAcademy/Pages/About.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages
+{
+    public partial class About
+    {
+        public string Title { get; set; } = "정보";
+        public string SubTitle { get; set; } = "부제목";
+
+        protected override void OnInitialized()
+        {
+            SubTitle = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
---
+++ VisualAcademy/Pages/Services.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace VisualAcademy.Pages
+{
+    public partial class Services
+    {
+        public string Title { get; set; } = "서비스";
+    }
+}
